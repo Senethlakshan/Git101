3ea02feeb05d56eb7b0a04a244dddd1942fdd1bb
Senethlakshan <kmseneth@gmail.com>
2024-04-24 17:23:20 +0530

    update book list
    update json book list new one
    
    Co-Authored-By: Malitha Sayuranga <122095159+malitha23@users.noreply.github.com>

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ app.use(express.json());
 // Dummy data for books
 let books = [
     { id: 1, title: 'Book 1', author: 'Author 1' },
-    { id: 2, title: 'Book 2', author: 'Author 2' }
+    { id: 2, title: 'Book 2', author: 'Author 2' },
+    { id: 3, title: 'Book 3', author: 'Author 2' }
 ];
 
 // GET all books
